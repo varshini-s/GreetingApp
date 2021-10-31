0a932690f5d30b1d6b1b53be703e0df8a6aad70c
varshini <dreamvarshini@gmail.com>
2021-10-31 23:14:46 +0530

    ADDED: greeting application class

---
+++ src/main/java/com/bridgelabz/greetingcontroller/GreetingControllerApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingControllerApplication.class, args);
+	}
+
+}
