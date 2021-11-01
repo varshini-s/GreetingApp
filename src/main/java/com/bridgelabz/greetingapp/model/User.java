3bcfedcf143581c4a28f21e0383a2e47a55c7219
varshini <dreamvarshini@gmail.com>
2021-11-01 23:22:51 +0530

    ADDED: user class

--- src/main/java/com/bridgelabz/greetingapp/model/User.java
+++ src/main/java/com/bridgelabz/greetingapp/model/User.java
@@ -24,4 +24,14 @@ public class User
     {
         this.lastName= lastName;
     }
+
+	@Override
+	public String toString() 
+	{
+		String str = (firstName != null)? firstName+" ":"";
+		str+=(lastName!=null) ? lastName:"";
+		return str.trim();
+	}
+    
+    
 }
