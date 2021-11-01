aa1b50766a295966e7b4c1a65671252ca94afd7a
varshini <dreamvarshini@gmail.com>
2021-11-01 23:26:34 +0530

    MODIFIED: methods for greeting service

--- src/main/java/com/bridgelabz/greetingapp/service/IGreetingService.java
+++ src/main/java/com/bridgelabz/greetingapp/service/IGreetingService.java
@@ -8,9 +8,6 @@ import com.bridgelabz.greetingapp.model.User;
 public interface IGreetingService 
 {
 	Greeting addGreeting(User user);
-	Greeting getGreetingById(long id);
-	List<Greeting> getGreetingList();
-	Greeting editGreeting(String name1,String name2);
-	List<Greeting> deleteGreeting(String name);
+	
 
 }
