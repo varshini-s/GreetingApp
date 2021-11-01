4bb9d346f307a4e482d698e9479431e4b80a3df9
varshini <dreamvarshini@gmail.com>
2021-11-01 18:35:56 +0530

    ADDED: greeting controller with various http calls

---
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -0,0 +1,49 @@
+package com.bridgelabz.greetingapp.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.bridgelabz.greetingapp.model.Greeting;
+
+
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController 
+{
+    private static final String template="Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping(value = {"","/","/home"})
+    public Greeting greeting(@RequestParam(value="name",defaultValue="World") String name)
+    {
+        return new Greeting(counter.incrementAndGet(),String.format(template, name));
+    }
+
+    @GetMapping("/param/{name}")
+    public Greeting sayHelloParam(@PathVariable String name)
+    {
+        return new Greeting(counter.incrementAndGet(),String.format(template, name));
+    }
+    
+    @PostMapping("/post")
+    public Greeting sayHello(@RequestBody Greeting greeting)
+    {
+
+        return new Greeting(greeting.getId(),greeting.getMessage());
+    }
+	
+    @PutMapping("put/{name}")
+    public Greeting sayHello(@PathVariable String name)
+	{
+        
+		return new Greeting(counter.incrementAndGet(),String.format(template, name));
+    }
+}
